678e83822f035c81588dcf9684b6d57bf159adba
Gnatolog <robertadamyanpy@gmail.com>
2024-03-20 08:48:06 +0300

    add_front_vertical_analyze

--- module_analiz_img/model/model.py
+++ module_analiz_img/model/model.py
@@ -97,7 +97,7 @@ class Preprocessing(Worker):
 
     def convert_in_black_white(self):
         if self.type_image == 'front':
-            img_black_white = cv2.cvtColor(self.result_image,  cv2.COLOR_BGR2GRAY)
+            img_black_white = cv2.cvtColor(self.result_image, cv2.COLOR_BGR2GRAY)
             self.result_image = img_black_white
             print("Convert black white image successful")
         elif self.type_image == 'lateral':
@@ -123,7 +123,7 @@ class Preprocessing(Worker):
     # endregion
 
 
-class Cuter(Worker):
+class Cuter:
     """
     Cropping the image by the area of interest
     Save image on server
@@ -131,16 +131,18 @@ class Cuter(Worker):
     """
 
     # region field
-
-    def __init__(self, name_img: str, path_field: str, type_image: str):
-        super().__init__(name_img, path_field, type_image)
+    def __init__(self, path_file):
+        self.path_file = path_file
 
     # endregion
 
     # region method
 
     def front_cut(self):
-        pass
+        img = cv2.imread(f'{self.path_file}result.jpg', flags=0)
+        img_resize = cv2.resize(img, (800, 600))  # resize
+        cropped = img_resize[200:400, 200:650]  # обрезка изображения image[y1:y2, x1:x2]
+        cv2.imwrite(f'{self.path_file}/result.jpg', cropped)
 
     def lateral_cut(self):
         pass
@@ -148,7 +150,7 @@ class Cuter(Worker):
     # endregion
 
 
-class Analyzer(Worker):
+class Analyzer:
     """
 
     Analiz image and getting the result
@@ -156,17 +158,116 @@ class Analyzer(Worker):
     """
 
     # region field
-    def __init__(self, name_img: str, path_field: str, type_image: str):
-        super().__init__(name_img, path_field, type_image)
+    def __init__(self, path_file):
+        self.path_file = path_file
 
     # endregion
 
     # region method
-    def front_analyze_vert(self):
-        pass
+    def front_analyze_vert(self) -> list[bool, int]:
+
+        img = cv2.imread(f'{self.path_file}result.jpg')
+        width = img.shape[0]  # 200
+        height = img.shape[1]  # 450
+
+        # Определить центральные резцы в,ч
+        len_dent = 0
+        number_teeth = 0
+        max_width_dent_pix = {}
+
+        for x in range(height):
+            if img[width // 4][x][2] > 200:
+                len_dent += 1
+            elif len_dent > 2:
+                number_teeth += 1
+                max_width_dent_pix[number_teeth] = (len_dent, x)
+                len_dent = 0
+
+        # Поиск максимального значения в.ч
+        max_len_upper = 0
+        max_len_pix_upper = 0
+        for key in max_width_dent_pix.keys():
+            if max_width_dent_pix[key][0] > max_len_upper:
+                max_len_upper = max_width_dent_pix[key][0]
+                max_len_pix_upper = max_width_dent_pix[key][1]
+
+        # Определить расположение центра центральных резцов
+        # относительно центра центра центральных резцов верхней челюсти
+        max_len_pix_lower = 0
+        check_central_low = [False]  # список хранит значение находится ли резцы
+        # в пределах нормы или нет
+
+        for y in range(max_len_pix_upper - 20, max_len_pix_upper + 21):
+
+            if img[165][y][2] < 200:
+                if y == max_len_pix_upper:
+                    check_central_low.append(y)
+                    check_central_low[0] = True
+                    break
+                else:
+                    check_central_low[0] = True
+                    max_len_pix_lower = y
+
+        check_central_low.append(max_len_pix_lower)
+
+        # TODO удалить после отадки
+        # print(check_central_low)
+
+        for y in range(width):  # [y][x]
+            for x in range(height):  # порводим перпедикуляр по центру верхних зубов
+                img[y][max_len_pix_upper] = 0
+
+        for y in range(100, 200):  # [y][x]
+            for x in range(height):  # проводим перпедикуляр по центру нижних зубов зубов
+                img[y][max_len_pix_lower] = 0
+
+        # TODO удалить после отадки
+        # print(f'{max_len_pix_upper= }\n{check_central_low[1]= }')
+
+        # TODO удалить после перееноса в результер
+
+        # cv2.imshow('cropped', img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        return check_central_low
 
     def front_analyze_hor(self):
-        pass
+
+        img = cv2.imread(f'{self.path_file}result.jpg')
+        height_dent_1_1 = 0  # длина первого резца вч
+        count_pixcel_dent_all = 0  # общая длина зубов
+        count_dent = 0
+        width = img.shape[0]  # 200
+        height = img.shape[1]  # 450
+        # print(f'{width= }')
+        # print(f'{height= }')
+
+        for i in range(width):
+            # print(img[i][185:190][1][1])
+            if img[i][185:190][1][1] > 160:
+                count_pixcel_dent_all += 1
+            if img[i][185:190][1][1] < 200 and (50 < i < 116):
+                height_dent_1_1 = i
+                img[i][:height] = 0
+
+        height_dent_4_1 = count_pixcel_dent_all - height_dent_1_1
+        part_upper_dent = round(height_dent_1_1 / count_pixcel_dent_all, 1)
+        part_lowwer_dent = round(height_dent_4_1 / count_pixcel_dent_all, 1)
+        print(f'{height_dent_4_1= }')
+        print(f'{height_dent_1_1= }')
+        print(f'{part_upper_dent= }')
+        print(f'{part_lowwer_dent= }')
+
+        result_analiz = part_upper_dent - part_lowwer_dent  # переменная хранить данные анализа
+
+        if result_analiz > 0.2:
+            print("У вас глубокий прикус")
+        elif 0 < result_analiz <= 0.2:
+            print("У вас нормальный прикус")
+        elif result_analiz == 0:
+            print("У вас прямой прикус")
+        elif result_analiz < 0:
+            print("У вас открытый прикус")
 
     def lateral_analyze_vert(self):
         pass
@@ -176,7 +277,7 @@ class Analyzer(Worker):
     # endregion
 
 
-class Resulter(Worker):
+class Resulter:
     """
 
     Interpretation of the analysis result
@@ -184,24 +285,36 @@ class Resulter(Worker):
     """
 
     # region field
-
-    def __init__(self, name_img: str, path_field: str, type_image: str):
-        super().__init__(name_img, path_field, type_image)
+    def __init__(self, name_user: str, id_user: int, results: dict, template: dict):
+        self.name_user = name_user
+        self.id_user = id_user
+        self.results = results
+        self.result_diagnostic = template
+        self.eval_data = {}
 
     # endregion
 
     # region method
-
-    def load_result_data(self):
-        pass
-
     def evaluation_data(self):
-        pass
+        # fill in the name, id
+        self.result_diagnostic.update({'name': self.name_user})
+        self.result_diagnostic.update({'id': self.id_user})
 
-    def save_result(self):
-        pass
+        # analiz front vertical
+        result_front_vertical = self.results['front_vert']
 
-    def get_path_result(self):
-        pass
+        if result_front_vertical[0] is True:
+            self.eval_data['front_vert'] = True
+            self.result_diagnostic['result_front_vertical'] = \
+                'У Вас не обнаружено смещение нижней челюсти'
+        else:
+            self.eval_data['front_vert'] = False
+            self.result_diagnostic['result_front_vertical'] = \
+                'У Вас обнаружено смещение нижней челюсти'
+
+        print('Diagnostic complete')
+
+    def get_result(self):
+        return self.result_diagnostic
 
     # endregion
--- module_analiz_img/presenter/presenter.py
+++ module_analiz_img/presenter/presenter.py
@@ -1,5 +1,7 @@
 from model import model
 from viewer import viewer
+import json
+from pathlib import Path
 
 
 # TODO Добавить эндпоинт по запуску процесса
@@ -16,16 +18,20 @@ class Startprocession:
         self.name_user = name_user
         self.id_user = id_user
         self.task_id = task_id
+        self.result_analiz = {}
 
     # endregion
 
     # region method
 
     def start_analiz(self):
+        # Load template
+        template = viewer.Vieweresulter().get_result_view()
 
         # load_file = model.Loader()   # загрузка файла из хранилища
 
-        preprocessing = model.Preprocessing('right_side.JPG',
+        # Preprocessing
+        preprocessing = model.Preprocessing('low_pricus.JPG',
                                             '/home/gnatolog/check_your_smile/'
                                             'module_analiz_img/temp_storage/',
                                             'front')
@@ -35,9 +41,31 @@ class Startprocession:
         preprocessing.convert_in_black_white()
         preprocessing.check_dent()
 
+        cute = model.Cuter('/home/gnatolog/check_your_smile/'
+                           'module_analiz_img/temp_storage/')
+
+        cute.front_cut()
+
+        # Analiz
+
+        analiz = model.Analyzer('/home/gnatolog/check_your_smile/'
+                                'module_analiz_img/temp_storage/')
+
+        # analiz.front_analyze_hor()
+        self.result_analiz['front_vert'] = analiz.front_analyze_vert()
+
+        # Interpretations analiz
+
+        interprit = model.Resulter(name_user=self.name_user, id_user=self.id_user,
+                                   results=self.result_analiz, template=template)
+        interprit.evaluation_data()
+
+        result_file = Path.cwd() / 'result_diagnostic'/f'result_diagnostic_{self.name_user}.json'
+        with open(result_file, 'w', encoding='utf-8') as fr:
+            json.dump(interprit.get_result(), fr, indent=2, ensure_ascii=False)
+
+
     def get_result(self):
         pass
 
     # endregion
-
-
